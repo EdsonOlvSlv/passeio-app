f4f8a747a86d44af049fa4b16674bb87da7cccc4
Edson Oliveira Silva <edson.o.s2011@gmail.com>
2025-09-19 19:46:52 -0300

    164919092025

--- src/app/lugares/lugar.service.ts
+++ src/app/lugares/lugar.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Lugar } from './lugar';
 import { Observable } from 'rxjs';
@@ -15,5 +15,23 @@ export class LugarService {
 
   obterTodas(): Observable<Lugar[]> {
     return this.http.get<Lugar[]>('http://localhost:3000/lugares');
+  }
+
+  filtrar(nome: string, categoria: string): Observable<Lugar[]> {
+    let parametros = new HttpParams();
+
+    /*http://localhost:3000/lugares?categoria_like=praias*/
+
+    if (nome) {
+      parametros = parametros.set('nome_like', nome);
+    }
+
+    if (categoria && categoria !== '-1') {
+      parametros = parametros.set('categoria', categoria);
+    }
+
+    return this.http.get<Lugar[]>('http://localhost:3000/lugares', {
+      params: parametros,
+    });
   }
 }
